42f268f152bd54771ad256b88620edd22a58580a
Raghav Jindal <raghavjindal131@gmail.com>
2021-03-25 09:15:25 +0530

    Updated the UI of the app

--- index.php
+++ index.php
@@ -2,24 +2,23 @@
 <style>
 	body#login::before {
     content: "";
-    background: #00000036;
-    position: absolute;
-    top: 0;
+    background: #222000036;
+    position: left;
+    top: 50px;
     /* z-index: 1; */
-    left: 0;
-    width: 100%;
-    height: 100%;
+    left: 10px;
+  
 }
 	
 </style>
 <body id="login">
     <div class="container" style="position: relative">
 		<div class="row-fluid">
-			<div class="span6"><div class="title_index"><?php include('title_index.php'); ?></div></div>
+			<div class="span6"><div class="title_index"><?php include('title.php'); ?></div></div>
 			<div style="margin-right:-400px;" class="span6"><div class="pull-right"><?php include('login_form.php'); ?></div></div>
 		</div>
 		<div class="row-fluid">
-			<div class="span12"><div class="index-footer"><?php include('link.php'); ?></div></div>
+			<div class="span12"><div class="index-footer"><?php include('link_home.php'); ?></div></div>
 		</div>
 			<?php include('footer.php'); ?>
     </div>
